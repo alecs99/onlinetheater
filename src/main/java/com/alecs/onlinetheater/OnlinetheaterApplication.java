a4b868e4c9b5d56569ad6ee3f0f882610fa9d606
alecs99 <ciobanu.alecsandru@gmail.com>
2021-12-20 20:01:49 +0200

    Initial commit

---
+++ src/main/java/com/alecs/onlinetheater/OnlinetheaterApplication.java
@@ -0,0 +1,13 @@
+package com.alecs.onlinetheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinetheaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinetheaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alecs/onlinetheater/OnlinetheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alecs.onlinetheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinetheaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
